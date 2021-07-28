cf281c6fb91dc47ecfff63683c25ca6a4b6f1481
Carlos Ruiz <cruiz@kionetworks.com>
2021-07-28 01:36:10 -0500

    Fixing nginx config

--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,10 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const axios_instance = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'http://172.16.5.171:8000/api/',
   headers: {
-    'Content-type': 'application/json'
-  }
-})
+    'Content-type': 'application/json',
+  },
+});
 
-export default axios_instance
+export default axios_instance;
